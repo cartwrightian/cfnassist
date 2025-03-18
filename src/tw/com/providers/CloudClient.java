01babfa7e1ea1ee54064446b55bdf87b3df39be3
Ian Cartwright <ian.cartwright@softwire.com>
2025-03-18 09:59:22 +0000

    small tidy up

--- src/tw/com/providers/CloudClient.java
+++ src/tw/com/providers/CloudClient.java
@@ -16,15 +16,15 @@ import static java.lang.String.format;
 public class CloudClient { //implements ProgressListener {
     private static final Logger logger = LoggerFactory.getLogger(CloudClient.class);
 
-    private Ec2Client ec2Client;
-    private AwsRegionProvider regionProvider;
+    private final Ec2Client ec2Client;
+    private final AwsRegionProvider regionProvider;
 
     public CloudClient(Ec2Client ec2Client, AwsRegionProvider regionProvider) {
         this.ec2Client = ec2Client;
         this.regionProvider = regionProvider;
     }
 
-    public Vpc describeVpc(String vpcId) {
+    public Vpc describeVpc(final String vpcId) {
         logger.debug("Get VPC by ID " + vpcId);
 
         DescribeVpcsRequest describeVpcsRequest = DescribeVpcsRequest.
@@ -32,7 +32,7 @@ public class CloudClient { //implements ProgressListener {
                 build();
 
         DescribeVpcsResponse results = ec2Client.describeVpcs(describeVpcsRequest);
-        return results.vpcs().get(0);
+        return results.vpcs().getFirst();
     }
 
     public List<Vpc> describeVpcs() {
@@ -42,45 +42,45 @@ public class CloudClient { //implements ProgressListener {
         return describeVpcsResults.vpcs();
     }
 
-    public void addTagsToResources(List<String> resources, List<Tag> tags) {
+    public void addTagsToResources(final List<String> resources, final List<Tag> tags) {
         CreateTagsRequest createTagsRequest = CreateTagsRequest.builder().resources(resources).tags(tags).build();
         ec2Client.createTags(createTagsRequest);
     }
 
-    public void deleteTagsFromResources(List<String> resources, Tag tag) {
+    public void deleteTagsFromResources(final List<String> resources, final Tag tag) {
         DeleteTagsRequest deleteTagsRequest = DeleteTagsRequest.builder().resources(resources).tags(tag).build();
         ec2Client.deleteTags(deleteTagsRequest);
     }
 
     public Map<String, AvailabilityZone> getAvailabilityZones() {
-        Region region = regionProvider.getRegion();
+        final Region region = regionProvider.getRegion();
         logger.info("Get AZ for region " + region);
-        Filter filter = Filter.builder().name("region-name").values(region.id()).build();
+        final Filter filter = Filter.builder().name("region-name").values(region.id()).build();
         DescribeAvailabilityZonesRequest request = DescribeAvailabilityZonesRequest.builder().
                 filters(filter).build();
 
 
-        DescribeAvailabilityZonesResponse result = ec2Client.describeAvailabilityZones(request);
-        List<AvailabilityZone> zones = result.availabilityZones();
+        final DescribeAvailabilityZonesResponse result = ec2Client.describeAvailabilityZones(request);
+        final List<AvailabilityZone> zones = result.availabilityZones();
         logger.info(format("Found %s zones", zones.size()));
 
-        Map<String, AvailabilityZone> zoneMap = new HashMap<>();
+        final Map<String, AvailabilityZone> zoneMap = new HashMap<>();
         zones.forEach(zone -> zoneMap.put(zone.zoneName().replace(zone.regionName(), ""), zone));
         return zoneMap;
     }
 
-    public software.amazon.awssdk.services.ec2.model.Instance getInstanceById(String id) throws WrongNumberOfInstancesException {
-        DescribeInstancesRequest request = DescribeInstancesRequest.builder().instanceIds(id).build();
-        DescribeInstancesResponse result = ec2Client.describeInstances(request);
-        List<Reservation> res = result.reservations();
+    public software.amazon.awssdk.services.ec2.model.Instance getInstanceById(final String id) throws WrongNumberOfInstancesException {
+        final DescribeInstancesRequest request = DescribeInstancesRequest.builder().instanceIds(id).build();
+        final DescribeInstancesResponse result = ec2Client.describeInstances(request);
+        final List<Reservation> res = result.reservations();
         if (res.size() != 1) {
             throw new WrongNumberOfInstancesException(id, res.size());
         }
-        List<software.amazon.awssdk.services.ec2.model.Instance> ins = res.get(0).instances();
+        List<software.amazon.awssdk.services.ec2.model.Instance> ins = res.getFirst().instances();
         if (ins.size() != 1) {
             throw new WrongNumberOfInstancesException(id, ins.size());
         }
-        return ins.get(0);
+        return ins.getFirst();
     }
 
     public List<Vpc> getVpcs() {
@@ -123,7 +123,7 @@ public class CloudClient { //implements ProgressListener {
         return result.addresses();
     }
 
-    public void addIpsToSecGroup(String secGroupId, Integer port, List<InetAddress> addresses) {
+    public void addIpsToSecGroup(final String secGroupId, final Integer port, final List<InetAddress> addresses) {
         logger.info(format("Add addresses %s for port %s to group %s", addresses, port.toString(), secGroupId));
 
         AuthorizeSecurityGroupIngressRequest request = AuthorizeSecurityGroupIngressRequest.builder().
@@ -134,7 +134,7 @@ public class CloudClient { //implements ProgressListener {
         ec2Client.authorizeSecurityGroupIngress(request);
     }
 
-    public void deleteIpFromSecGroup(String groupId, Integer port, List<InetAddress> addresses) {
+    public void deleteIpFromSecGroup(final String groupId, final Integer port, final List<InetAddress> addresses) {
         logger.info(format("Remove addresses %s for port %s on group %s", addresses, port.toString(), groupId));
         RevokeSecurityGroupIngressRequest request = RevokeSecurityGroupIngressRequest.builder().
                 groupId(groupId).ipPermissions(createPermissions(port, addresses)).build();
@@ -144,7 +144,7 @@ public class CloudClient { //implements ProgressListener {
         ec2Client.revokeSecurityGroupIngress(request);
     }
 
-    private Collection<IpPermission> createPermissions(Integer port, List<InetAddress> addresses) {
+    private Collection<IpPermission> createPermissions(final Integer port, final List<InetAddress> addresses) {
 
         Collection<IpPermission> ipPermissions = new LinkedList<>();
         addresses.forEach(address ->{
@@ -167,7 +167,7 @@ public class CloudClient { //implements ProgressListener {
 //    }
 
     // unencrypted PEM encoded RSA private key
-    public AWSPrivateKey createKeyPair(String keypairName) {
+    public AWSPrivateKey createKeyPair(final String keypairName) {
         logger.info("Create keypair with name " + keypairName);
         CreateKeyPairRequest request = CreateKeyPairRequest.builder().keyName(keypairName).build();
         CreateKeyPairResponse result = ec2Client.createKeyPair(request);
